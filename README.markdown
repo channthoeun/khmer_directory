2e340204d5583c36e43862e9af1e88956b36115f
Channthoeun Ken <channthoeun@gmail.com>
2011-08-21 09:43:22 +0700

    Improved the README

---
+++ README.markdown
@@ -0,0 +1,4 @@
+#Khmer Business Directory Application
+
+This is the application for [*Business Directory in Khmer Language*]
+(http://www.eken.com.kh) by [Channthoeun Ken](http://www.eken.com.kh).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KhmerDirectory::Application.config.secret_token = 'b606c1c340c20350b4a85a6f3d4e3cccf0dc372401171ed4d249e02b4855959ab183f918535aa955c06d77d9adeb3c7522a66f52e2a5af22cd89287cb161fdc4'
